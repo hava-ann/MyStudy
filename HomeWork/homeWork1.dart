2266987227e0fa0c6f4453fc81f279fd44392e23
hava-ann <annihaver@gmail.com>
2022-05-18 14:27:45 +0300

    feat: Создать некий словарь (Дедушка, бабушка, мама, папа, брат, сестра, сын, дочь) и функцию в которую мы можешь написать слово из нашего словаря и функция выведет нам перевод на английском языке нашего слова: Пример вывода: “На английском языке слово Папа будет father”

--- HomeWork/homeWork1.dart
+++ HomeWork/homeWork1.dart
@@ -1,4 +1,11 @@
 void main() {
-  print("Hello world");
-  print("что за нах");
+  num(0);
+}
+
+void num(int a) {
+  if (a > 0 & a != 0) {
+    print("Число  $a является натуральным числом");
+  } else if (a < 0) {
+    print("Число $a является ненатуральным числом");
+  }
 }
